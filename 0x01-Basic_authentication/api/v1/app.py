5dde84723c9e359dcd7b99ccc8c842163f3f9861
Mbokola <tonypeaks@gmail.com>
2024-01-17 14:10:10 +0000

    Basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -15,8 +15,12 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = getenv("AUTH_TYPE", None)
 
 if auth:
-    from api.v1.auth.auth import Auth
-    auth = Auth()
+    if auth == 'basic_auth':
+        from  api.v1.auth.basic_auth import BasicAuth
+        auth = BasicAuth()
+    else:
+        from api.v1.auth.auth import Auth
+        auth = Auth()
 
 
 @app.before_request
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" basic_auth module
+"""
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    """ BasicAuth class definition
+    """
+    pass
