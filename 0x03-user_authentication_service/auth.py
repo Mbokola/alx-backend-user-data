eb78ecb8ebc38833d0bef3aaa85518923e82416e
Mbokola <tonypeaks@gmail.com>
2024-01-24 13:29:11 +0000

    add valid_login, _generate_uuid, create_session methods/functions

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,8 +4,10 @@
 from db import DB
 from user import User
 from sqlalchemy.exc import NoResultFound
+from typing import Union
 
 import bcrypt  # type: ignore
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
@@ -16,6 +18,12 @@ def _hash_password(password: str) -> bytes:
     salted_hashed_passwod = bcrypt.hashpw(password_byte_encoding, salt)
 
     return salted_hashed_passwod
+
+
+def _generate_uuid() -> str:
+    """ generates a uuid and returns it
+    """
+    return str(uuid.uuid4())
 
 
 class Auth:
@@ -36,3 +44,29 @@ class Auth:
             self._db.add_user(email, hashed_password)
             created_user = self._db.find_user_by(email=email)
             return created_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ Validatates password against hashed password using bcrypt
+        """
+        try:
+            existing_user = self._db.find_user_by(email=email)
+            password_byte_encoding = password.encode('utf-8')
+            result = bcrypt.checkpw(password_byte_encoding,
+                                    existing_user.hashed_password)
+
+            return result
+
+        except (ValueError, NoResultFound):
+            return False
+
+    def create_session(self, email: str) -> Union[str, None]:
+        """ Updates the session_id entry of a user in the database
+        """
+        try:
+            existing_user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            existing_user.session_id = session_id
+            self._db._session.commit()
+            return session_id
+        except NoResultFound:
+            return None
