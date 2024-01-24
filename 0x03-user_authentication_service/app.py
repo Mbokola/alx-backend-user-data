e6282a764ceb69a01be471a4f5583557e6ebef7a
Mbokola <tonypeaks@gmail.com>
2024-01-24 12:31:00 +0000

    add "/users" route to app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """ Flask application
 """
-from flask import Flask, jsonify
+from flask import abort, Flask, request, jsonify
+from auth import Auth
 
+
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -13,6 +16,32 @@ def root_path():
     return jsonify({
         "message": "Bienvenue"
         })
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users():
+    """ Handles registration of a user
+    """
+    try:
+        email = request.form.get("email")
+        password = request.form.get("password")
+        created_user = AUTH.register_user(email, password)
+
+        return jsonify({
+            "eamil": created_user.email,
+            "message": "user created"
+            })
+    except ValueError:
+        return abort(400)
+
+
+@app.errorhandler(400)  # type: ignore
+def registerd_user(error):
+    """ Handles error raised during user regisrtation
+    """
+    return jsonify({
+        "message": "email already registered"
+        }), 400
 
 
 if __name__ == "__main__":
