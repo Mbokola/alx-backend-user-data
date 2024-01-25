1d61ccfaba16b44dff6732badebe421720c70cd5
Mbokola <tonypeaks@gmail.com>
2024-01-25 13:06:18 +0000

    Debug

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
-"""defines a method _hash_password"""
-import bcrypt
-import uuid
+""" Auth module
+"""
 from db import DB
 from user import User
-from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import NoResultFound
+from typing import Union
+
+import bcrypt  # type: ignore
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
-    """takes a password and return its encoded bytes"""
+    """ Encrypts user password
+    """
+    password_byte_encoding = password.encode('utf-8')
     salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed_password
+    salted_hashed_passwod = bcrypt.hashpw(password_byte_encoding, salt)
+
+    return salted_hashed_passwod
