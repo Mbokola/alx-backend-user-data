bc953d32e0ee4646e92e40da2656f8e70797a851
Mbokola <tonypeaks@gmail.com>
2024-01-25 11:39:52 +0000

    Try something

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -43,7 +43,7 @@ class DB:
         self._session.commit()
         return user
 
-    def find_user_by(self, **kwargs: Union[int, str]) -> User:
+    def find_user_by(self, **kwargs) -> User:
         """ Finds a user from the database based on kwargs """
         try:
             records = self._session.query(User).filter_by(**kwargs).first()
