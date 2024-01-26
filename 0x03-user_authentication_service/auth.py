f4833814e84f86acf2b37221a1a9dff69cd918b5
Mbokola <tonypeaks@gmail.com>
2024-01-26 07:52:04 +0000

    Revert

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -41,12 +41,9 @@ class Auth:
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
             hashed_password = _hash_password(password)
-            self._db._session.add(User(
-                email=email,
-                hashed_password=hashed_password
-            ))
-            self._db._session.commit()
-            return self._db.find_user_by(email=email)
+            self._db.add_user(email, hashed_password)
+            created_user = self._db.find_user_by(email=email)
+            return created_user
 
     def valid_login(self, email: str, password: str) -> bool:
         """ Validatates password against hashed password using bcrypt
